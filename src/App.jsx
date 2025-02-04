ab5c9c05c8673c6105a63c1f1b11b5172e591e7f
riyudev <virtudazoy@gmail.com>
2025-02-04 20:44:21 +0800

    added pages links

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,30 @@
+import React from "react";
 import "./App.css";
 import Navbar from "./components/Navbar";
+import Shop from "./pages/Shop";
+import ShopCategory from "./pages/ShopCategory";
+import Product from "./pages/Product";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Cart from "./pages/Cart";
+import LoginSignup from "./pages/LoginSignup";
 
 function App() {
   return (
     <div className="">
-      <Navbar />
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Shop />} />
+          <Route path="/mens" element={<ShopCategory category="men" />} />
+          <Route path="/womens" element={<ShopCategory category="women" />} />
+          <Route path="/kids" element={<ShopCategory category="kid" />} />
+          <Route path="/product" element={<Product />}>
+            <Route path=":productId" element={<Product />} />
+          </Route>
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/login" element={<LoginSignup />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ src/pages/ShopCategory.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function ShopCategory() {
+  return (
+    <div>
+      <div></div>
+    </div>
+  );
+}
+
+export default ShopCategory;
