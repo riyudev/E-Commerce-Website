5bf4cc1a1e92b58665df20df31529aab03c6a6f5
riyudev <virtudazoy@gmail.com>
2025-02-17 23:42:01 +0800

    added product category images

--- src/assets/AllProducts.js
+++ src/assets/AllProducts.js
@@ -1,16 +1,41 @@
 import Men1 from "./men1.jpg";
 import Women1 from "./women1.jpg";
-import Kid1 from "./kid2.jpg";
+import Kid1 from "./kid1.jpg";
+import Men2 from "./men2.jpg";
+import Women2 from "./women2.jpg";
+import Kid2 from "./kid2.jpg";
+import Men3 from "./men3.jpg";
+import Women3 from "./women3.jpg";
+import Kid3 from "./kid3.jpg";
+import Men4 from "./men4.jpg";
+import Women4 from "./women4.jpg";
+import Kid4 from "./kid4.jpg";
+import Men5 from "./men5.jpg";
+import Women5 from "./women5.jpg";
+import Kid5 from "./kid5.png";
+import Men6 from "./men6.jpg";
+import Women6 from "./women6.jpg";
+import Kid6 from "./kid6.png";
+import Men7 from "./men7.jpg";
+import Women7 from "./women7.jpg";
+import Kid7 from "./kid7.png";
+import Men8 from "./men8.jpg";
+import Women8 from "./women8.jpg";
+import Kid8 from "./kid8.png";
+import Men9 from "./men9.png";
+import Women9 from "./women9.png";
+import Kid9 from "./kid9.png";
+import Men10 from "./men10.png";
+import Women10 from "./women10.png";
+import Kid10 from "./kid10.png";
+import Men11 from "./men11.png";
+import Women11 from "./women11.png";
+import Kid11 from "./kid11.png";
+import Men12 from "./men12.png";
+import Women12 from "./women12.png";
+import Kid12 from "./kid12.png";
 
 let allProduct = [
-  {
-    id: 1,
-    name: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-    category: "women",
-    image: Women1,
-    newPrice: 50,
-    oldPrice: 80,
-  },
   {
     id: 1,
     name: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
@@ -20,10 +45,282 @@ let allProduct = [
     oldPrice: 80,
   },
   {
-    id: 1,
+    id: 2,
+    name: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    category: "women",
+    image: Women1,
+    newPrice: 50,
+    oldPrice: 80,
+  },
+  {
+    id: 3,
     name: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
     category: "kid",
     image: Kid1,
+    newPrice: 50,
+    oldPrice: 80,
+  },
+  {
+    id: 4,
+    name: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    category: "men",
+    image: Men2,
+    newPrice: 50,
+    oldPrice: 80,
+  },
+  {
+    id: 5,
+    name: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    category: "women",
+    image: Women2,
+    newPrice: 50,
+    oldPrice: 80,
+  },
+  {
+    id: 6,
+    name: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    category: "kid",
+    image: Kid2,
+    newPrice: 50,
+    oldPrice: 80,
+  },
+  {
+    id: 7,
+    name: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    category: "men",
+    image: Men3,
+    newPrice: 50,
+    oldPrice: 80,
+  },
+  {
+    id: 8,
+    name: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    category: "women",
+    image: Women3,
+    newPrice: 50,
+    oldPrice: 80,
+  },
+  {
+    id: 9,
+    name: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    category: "kid",
+    image: Kid3,
+    newPrice: 50,
+    oldPrice: 80,
+  },
+  {
+    id: 10,
+    name: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    category: "men",
+    image: Men4,
+    newPrice: 50,
+    oldPrice: 80,
+  },
+  {
+    id: 11,
+    name: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    category: "women",
+    image: Women4,
+    newPrice: 50,
+    oldPrice: 80,
+  },
+  {
+    id: 12,
+    name: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    category: "kid",
+    image: Kid4,
+    newPrice: 50,
+    oldPrice: 80,
+  },
+  {
+    id: 13,
+    name: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    category: "men",
+    image: Men5,
+    newPrice: 50,
+    oldPrice: 80,
+  },
+  {
+    id: 14,
+    name: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    category: "women",
+    image: Women5,
+    newPrice: 50,
+    oldPrice: 80,
+  },
+  {
+    id: 15,
+    name: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    category: "kid",
+    image: Kid5,
+    newPrice: 50,
+    oldPrice: 80,
+  },
+  {
+    id: 16,
+    name: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    category: "men",
+    image: Men6,
+    newPrice: 50,
+    oldPrice: 80,
+  },
+  {
+    id: 17,
+    name: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    category: "women",
+    image: Women6,
+    newPrice: 50,
+    oldPrice: 80,
+  },
+  {
+    id: 18,
+    name: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    category: "kid",
+    image: Kid6,
+    newPrice: 50,
+    oldPrice: 80,
+  },
+  {
+    id: 19,
+    name: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    category: "men",
+    image: Men7,
+    newPrice: 50,
+    oldPrice: 80,
+  },
+  {
+    id: 20,
+    name: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    category: "women",
+    image: Women7,
+    newPrice: 50,
+    oldPrice: 80,
+  },
+  {
+    id: 21,
+    name: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    category: "kid",
+    image: Kid7,
+    newPrice: 50,
+    oldPrice: 80,
+  },
+  {
+    id: 22,
+    name: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    category: "men",
+    image: Men8,
+    newPrice: 50,
+    oldPrice: 80,
+  },
+  {
+    id: 23,
+    name: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    category: "women",
+    image: Women8,
+    newPrice: 50,
+    oldPrice: 80,
+  },
+  {
+    id: 24,
+    name: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    category: "kid",
+    image: Kid8,
+    newPrice: 50,
+    oldPrice: 80,
+  },
+  {
+    id: 25,
+    name: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    category: "men",
+    image: Men9,
+    newPrice: 50,
+    oldPrice: 80,
+  },
+  {
+    id: 26,
+    name: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    category: "women",
+    image: Women9,
+    newPrice: 50,
+    oldPrice: 80,
+  },
+  {
+    id: 27,
+    name: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    category: "kid",
+    image: Kid9,
+    newPrice: 50,
+    oldPrice: 80,
+  },
+  {
+    id: 28,
+    name: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    category: "men",
+    image: Men10,
+    newPrice: 50,
+    oldPrice: 80,
+  },
+  {
+    id: 29,
+    name: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    category: "women",
+    image: Women10,
+    newPrice: 50,
+    oldPrice: 80,
+  },
+  {
+    id: 30,
+    name: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    category: "kid",
+    image: Kid10,
+    newPrice: 50,
+    oldPrice: 80,
+  },
+  {
+    id: 31,
+    name: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    category: "men",
+    image: Men11,
+    newPrice: 50,
+    oldPrice: 80,
+  },
+  {
+    id: 32,
+    name: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    category: "women",
+    image: Women11,
+    newPrice: 50,
+    oldPrice: 80,
+  },
+  {
+    id: 33,
+    name: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    category: "kid",
+    image: Kid11,
+    newPrice: 50,
+    oldPrice: 80,
+  },
+  {
+    id: 34,
+    name: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    category: "men",
+    image: Men12,
+    newPrice: 50,
+    oldPrice: 80,
+  },
+  {
+    id: 35,
+    name: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    category: "women",
+    image: Women12,
+    newPrice: 50,
+    oldPrice: 80,
+  },
+  {
+    id: 36,
+    name: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    category: "kid",
+    image: Kid12,
     newPrice: 50,
     oldPrice: 80,
   },
--- src/pages/ShopCategory.jsx
+++ src/pages/ShopCategory.jsx
@@ -1,11 +1,32 @@
 import React, { useContext } from "react";
 import { ShopContext } from "../context/ShopContext";
+import Item from "../components/Items";
 
 const ShopCategory = (props) => {
+  const { allProduct } = useContext(ShopContext);
+
   return (
     <div className="place-items-center">
-      <div className="mt-24">
+      <div className="mt-24 mb-10">
         <img src={props.banner} alt="" />
+      </div>
+      <div>
+        {allProduct.map((item, i) => {
+          if (props.category === item.category) {
+            return (
+              <Item
+                key={i}
+                id={item.id}
+                name={item.name}
+                image={item.image}
+                newPrice={item.newPrice}
+                oldPrice={item.oldPrice}
+              />
+            );
+          } else {
+            return null;
+          }
+        })}
       </div>
     </div>
   );
