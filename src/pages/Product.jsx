1547dff7eb675e16ba39252bd9c74e6758664e35
riyudev <virtudazoy@gmail.com>
2025-03-22 19:34:41 +0800

    added product.jsx

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,6 +1,12 @@
-import React from "react";
+import React, { useContext } from "react";
+import { ShopContext } from "../context/ShopContext";
+import { useParams } from "react-router-dom";
 
 function Product() {
+  const { allProducts } = useContext(ShopContext);
+  const { productId } = useParams();
+  const product = allProducts.find((e) => e.id === Number(productId));
+
   return (
     <div>
       <div></div>
