b1213f0af529a1f5452fe6dac56f603ed249bfdb
riyudev <virtudazoy@gmail.com>
2025-04-23 14:32:11 +0800

    responsive Popular

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -5,10 +5,10 @@ import Item from "./Items";
 const Popular = () => {
   return (
     <div className="flex items-center justify-center">
-      <div className="w-[90%] place-items-center space-y-4">
+      <div className="laptop:w-[90%] w-[95%] place-items-center space-y-4">
         <h3>POPULAR IN WOMEN</h3>
         <hr className="w-40 border-[3px] border-amber-400" />
-        <div className="flex space-x-6">
+        <div className="laptop:grid-cols-4 tablet:gap-x-6 grid w-auto grid-cols-2 gap-x-3">
           {dataProduct.map((item, i) => {
             return (
               <Item
